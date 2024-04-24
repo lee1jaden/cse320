75f823693bac8baeafbbd2af27f314ed47cdf9e6
lee1jaden_MSIModern <jadenlee1@att.net>
2024-04-24 11:24:30 +0900

    Prompts user for text and word strings.

--- as6.c
+++ as6.c
@@ -13,6 +13,8 @@
 #include <string.h>
 #include <time.h>
 
+const int max_string_length = 100;
+
 void* word_detect_join(void *vargp);
 void* word_detect_detach(void *vargp);
 
@@ -23,31 +25,53 @@ clock_t detach_end_time;
 
 int main() {
     pthread_t tid = pthread_self();
-    printf("main: %u\n", (unsigned int)tid);
-    void *ret;
+    printf("Main Thread ID: %u\n", (unsigned int)tid);
+    char text_string[max_string_length + 2];
+    char target_word[max_string_length + 2];
 
     while(1) {
-        char str[100];
-        pthread_t tid;
-        scanf("%99s", str);
-        if(strcmp(str, "quit") == 0)
+        // Prompt user to enter text. Check if the string is too long or contains 'quit'.
+        printf("Enter some text or quit: ");
+        fgets(text_string, max_string_length + 2, stdin);
+        if (strlen(text_string) > max_string_length && text_string[strlen(text_string) - 1] != '\n') {
+            printf("Error: Text string was longer than 100 characters long. Now exiting.\n");
+            exit(EXIT_FAILURE);
+        }
+        text_string[strlen(text_string) - 1] = 0;
+        if(strcmp(text_string, "quit") == 0)
             break;
 
-        // TODO: Create a peer thread for word_detect_join, detecting a word in the str buffer using pthread_join
-        pthread_t joinThread;
-        pthread_create(&joinThread, NULL, &word_detect_join, str);
-        pthread_join(joinThread, &ret);
-        float runtime = *((float*)ret);
-        printf("Join thread ran for %.6f seconds.\n", runtime);
+        // printf("String '%s' is length %d\n", text_string, (int)strlen(text_string)); //DEBUG
 
+        // Prompt user to enter the word to search for. Check if the string is too long or has multiple words.
+        printf("Enter a word you'd like to detect: ");
+        fgets(target_word, max_string_length + 2, stdin);
+        if (strlen(target_word) > max_string_length && target_word[strlen(target_word) - 1] != '\n') {
+            printf("Error: Word was longer than 100 characters long. Now exiting.\n");
+            exit(EXIT_FAILURE);
+        }
+        target_word[strlen(target_word) - 1] = 0;
+        // printf("String '%s' is length %d\n", target_word, (int)strlen(target_word)); //DEBUG
+        if (strchr(target_word, ' ') != NULL) {
+            printf("Error: Submitted more than one word. Now exiting.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        // printf("You want to detect '%s' from '%s'?\n", target_word, text_string); //DEBUG
+
+        // TODO: Create a peer thread for word_detect_join, detecting a word in the str buffer using pthread_join
+        // ...
         // TODO: Create a peer thread for word_detect_detach, detecting a word in the str buffer using pthread_detach
         // ...
     }
     // TODO: Free the str buffer if you dynamically allocate the string memory
-    free(ret);
+
     return 0;
 }
 
+/// @brief 
+/// @param vargp - text string to search through.
+/// @return NULL
 void* word_detect_join(void *vargp) {
     // TODO: Implement a peer thread using pthread_join to detect a word in the argument passed
     // TODO: Print the detected word and frequency if there are multiple detections
